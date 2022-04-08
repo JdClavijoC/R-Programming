b673291732ab0a90008ed4bc00a5d3a439041094
Jesus David <jdclavijoc@ufpso.edu.co>
2022-04-08 08:03:40 -0500

    vectors and more

---
+++ R_Basics/Vectors_and_more.R
@@ -0,0 +1,17 @@
+vector1 <- c(32,4654,876,52,6532)
+vector2 <- c(321L, 3443L, 5L, 432L)
+is.numeric(vector1)
+is.numeric(vector2)
+is.integer(vector1)
+is.integer(vector2)
+is.double(vector1)
+is.double(vector2)
+
+vector3 <- c("da","fdsc","gfeko", 9)
+is.numeric(vector3)
+
+seq(1,20,2)
+
+rep(3, 10)
+rep(seq(10),10)
+c(80,20)
